633adc2281f34addcec5d9eecef9e5cc028a245b
perryBunn <eagle4ever20@gmail.com>
2021-02-03 00:25:49 -0600

    fucking around

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 let randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
-let body = document.getElementById("body");
-body.style.setProperty("--main-bg-color", randomColor, "important")
+let element = document.getElementById("randomColor");
+element.style.setProperty("background-color", randomColor, "important")
+
 
